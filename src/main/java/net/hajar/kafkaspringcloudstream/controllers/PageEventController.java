c5f09aeabbe20e832be63f9f950c468d402d7961
HAJAR ATYQ <hajar.atyq.pro@gmail.com>
2025-12-24 13:49:30 +0100

    SC

--- src/main/java/net/hajar/kafkaspringcloudstream/controllers/PageEventController.java
+++ src/main/java/net/hajar/kafkaspringcloudstream/controllers/PageEventController.java
@@ -1,6 +1,7 @@
 package net.hajar.kafkaspringcloudstream.controllers;
 
 import net.hajar.kafkaspringcloudstream.events.PageEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,17 @@ import java.util.stream.Stream;
 
 @RestController
 public class PageEventController {
-    private StreamBridge streamBridge;
+    @Autowired
+    private StreamBridge streamBridge;   // permet de publier un message sur un broker
 
     @GetMapping("/publish")
     public PageEvent publish(String name, String topic) {
-        PageEvent event =new PageEvent(
+        PageEvent event = new PageEvent(
                 name,
                 Math.random()>0.5?"U1":"U2",
                 new Date(),
                 10 + new Random().nextLong(10000));
+        streamBridge.send(topic, event);
         return  event;
     }
 }
--- src/main/java/net/hajar/kafkaspringcloudstream/handlers/PageEventHandler.java
+++ src/main/java/net/hajar/kafkaspringcloudstream/handlers/PageEventHandler.java
@@ -2,10 +2,20 @@ package net.hajar.kafkaspringcloudstream.handlers;
 
 import net.hajar.kafkaspringcloudstream.controllers.PageEventController;
 import net.hajar.kafkaspringcloudstream.events.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 @Component
 public class PageEventHandler {
@@ -18,4 +28,26 @@ public class PageEventHandler {
             System.out.println("*****************");
         };
     }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier () {
+        return ()->{
+            return new PageEvent(
+                    Math.random()>0.5?"P1":"P2",Math.random()>0.5?"U1":"U2",new Date(),10+new Random().nextLong(10000)
+            );
+        };
+    }
+
+    @Bean
+    public Function <KStream<String,PageEvent>,KStream<String,Long>> kStreamFunction() {
+            return (input)->
+                                             input.filter((k,v)->v.duration()>100)
+                                                     .map((k,v)->new KeyValue<>(v.name(), v.duration()))
+                                                     .groupByKey(Grouped.with(Serdes.String(),Serdes.Long()))
+                                                     .windowedBy(TimeWindows.of(Duration.ofSeconds(5000)))
+                                                     .count()
+                                                     .toStream()
+                                                     .map((k,v)-> new KeyValue<>(k.key(),v));
+    }
+
 }
